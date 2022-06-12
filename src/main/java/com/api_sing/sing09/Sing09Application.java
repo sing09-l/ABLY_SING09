83f021587e9242e9ce18dee1bfb2bbf4bd3dfdb9
sing09 <mixberry0929@gmail.com>
2022-06-12 23:30:09 +0900

    commit ba

---
+++ src/main/java/com/api_sing/sing09/Sing09Application.java
@@ -0,0 +1,13 @@
+package com.api_sing.sing09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sing09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sing09Application.class, args);
+	}
+
+}
